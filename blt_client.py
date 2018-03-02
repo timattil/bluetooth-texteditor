10429679772f592ad378980a5a1cea32b365a79c
TTO200120\artnieme <arttu.niemela@oulu.fi>
2018-03-02 20:10:23 +0200

    blt_client Basic bl comms work. Need to connect text_editor to queues now

---
+++ blt_client.py
@@ -0,0 +1,169 @@
+import tkinter as tk
+from bluetooth import *
+import threading as th
+import hashlib
+import queue
+from time import gmtime, strftime, sleep
+
+def now():
+    return strftime('%H:%M:%S', gmtime())
+
+class Text_editor(tk.Tk):
+    def __init__(self, send_queue, recv_queue):
+        tk.Tk.__init__(self)
+        self.set_text_window()
+
+    def set_text_window(self):
+        self.text_window = Text_window(self)
+        self.text_window.grid()
+        self.text_window.start()
+
+class Text_window(tk.Text):
+    def __init__(self, parent):
+        tk.Text.__init__(self, parent)
+        self.parent = parent
+        self.last_hash = self.get_hash()
+
+    def start(self):
+        self.bindings()
+        self.set_last()
+        self.last_written()
+
+    def bindings(self):
+        self.bind('<BackSpace>', self.text_deleted)
+        self.bind('<Control-x>', self.text_cut)
+
+    def set_last(self):
+        self.mark_set('last', 'insert')
+        self.mark_gravity('last', 'left')
+
+    def print_out(func):
+        def wrapped_function(self, *args, **kwargs):
+            text = func(self, *args, **kwargs)
+            if text:
+                print('{}: {} @ {}'.format(now(), text[0], text[1]))
+        return wrapped_function
+
+    def get_hash(self):
+        return hashlib.md5(self.get('1.0', 'end').encode('utf-8')).digest()
+
+    def has_changed(self):
+        if self.last_hash != self.get_hash():
+            return True
+        else:
+            return False
+
+    @print_out
+    def last_written(self):
+        last_text = self.get('last', 'insert')
+        last_index = self.index('last')
+        if last_text and self.has_changed():
+            out = (last_text, last_index)
+            self.last_hash = self.get_hash()
+        else:
+            out = None
+        self.parent.after(10, self.last_written)
+        self.set_last()
+        return out
+
+    def text_deleted(self, *args):
+        try:
+            _from = self.index('sel.first')
+            _to = self.index('self.last')
+        except tk.TclError:
+            _from = 'insert -1 chars'
+            _to = 'insert'
+        deleted_char = self.get(_from, _to)
+        print(now(), 'DELETED: {} @ {} {}'.format(deleted_char, self.index(_from), self.index(_to)))
+        self.delete(_from, _to)
+        return 'break'
+
+    def text_cut(self, *args):
+        try:
+            _from = self.index('sel.first')
+            _to = self.index('self.last')
+        except tk.TclError:
+            print(now(), 'Cut: no selection')
+            return 'break'
+        deleted_char = self.get(_from, _to)
+        self.parent.clipboard_clear()
+        self.parent.clipboard_append(deleted_char)
+        print(now(), "CUT: {} @ {} {}".format(deleted_char, self.index(_from), self.index(_to)))
+        self.delete(_from. _to)
+        return 'break'
+
+class Bluetooth_comms(th.Thread):
+    def __init__(self, send_queue, recv_queue):
+        th.Thread.__init__(self)
+        self._stop_event = th.Event()
+        self.start()
+
+    def run(self):
+        try:
+            while not self._stop_event.is_set():
+                self.send()
+                self.receive()
+                print('bl_comms')
+                sleep(1)
+        except IOError:
+            pass
+
+    def stop(self):
+        self._stop_event.set()
+
+    def send(self):
+        try:
+            data = send_queue.get_nowait()
+            print(data)
+            send_queue.task_done()
+        except queue.Empty:
+            pass
+
+    def receive(self):
+        try:
+            data = recv_queue.get_nowait()
+            print(data)
+            recv_queue.task_done()
+        except queue.Empty:
+            pass
+
+def start_bluetooth():
+    addr = None
+
+    print("Searching all nearby bluetooth devices for the BLT Host.")
+
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+    service_matches = find_service( uuid = uuid, address = addr )
+
+    if len(service_matches) == 0:
+        print("Couldn't find the BLT Host =(")
+        sys.exit(0)
+
+    first_match = service_matches[0]
+    port = first_match["port"]
+    name = first_match["name"]
+    host = first_match["host"]
+
+    print("Connecting to \"%s\" on %s" % (name, host))
+
+    # Create the client socket
+    sock = BluetoothSocket( RFCOMM )
+    sock.connect((host, port))
+
+    return sock
+
+if __name__ == '__main__':
+    sock = start_bluetooth()
+
+    send_queue = queue.Queue()
+    recv_queue = queue.Queue()
+
+    bl_comms = Bluetooth_comms(send_queue, recv_queue)
+
+    text_editor = Text_editor(send_queue, recv_queue)
+    text_editor.mainloop()
+
+    bl_comms.stop()
+    bl_comms.join()
+
+    print('allsgood')
--- client.py
+++ client.py
@@ -1,48 +1,157 @@
-# file: rfcomm-client.py
-# auth: Albert Huang <albert@csail.mit.edu>
-# desc: simple demonstration of a client application that uses RFCOMM sockets
-#       intended for use with rfcomm-server
-#
-# $Id: rfcomm-client.py 424 2006-08-24 03:35:54Z albert $
-
 from bluetooth import *
+import threading
 import sys
+import time
+import texteditor
 
-if sys.version < '3':
-    input = raw_input
+class Receiver(threading.Thread):
+    def __init__(self, sock, program):
+        threading.Thread.__init__(self)
+        self.text_editor = program
+        self._stop_event = threading.Event()
+        self.start()
 
-addr = None
+    def run(self):
+        try:
+            while not self._stop_event.is_set():
+                data = sock.recv(1024)
+                if len(data) != 0:
+                    self.text_editor.text_window.insert('last', data)
+                    print('Received {}'.format(data))
+        except IOError:
+            pass
 
-if len(sys.argv) < 2:
-    print("no device specified.  Searching all nearby bluetooth devices for")
-    print("the SampleServer service")
-else:
-    addr = sys.argv[1]
-    print("Searching for SampleServer on %s" % addr)
+    def stop(self):
+        self._stop_event.set()
 
-# search for the SampleServer service
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-service_matches = find_service( uuid = uuid, address = addr )
+class Sender(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self._stop_event = threading.Event()
+        self.start()
 
-if len(service_matches) == 0:
-    print("couldn't find the SampleServer service =(")
-    sys.exit(0)
+    def run(self):
+        try:
+            while not self._stop_event.is_set():
+                data = sock.send(1024)
+                if len(data) != 0:
+                    self.text_editor.text_window.insert('last', data)
+                    print('Received {}'.format(data))
+        except IOError:
+            pass
 
-first_match = service_matches[0]
-port = first_match["port"]
-name = first_match["name"]
-host = first_match["host"]
+    def stop(self):
+        self._stop_event.set()
 
-print("connecting to \"%s\" on %s" % (name, host))
+if __name__ == '__main__':
+    if sys.version < '3':
+        input = raw_input
 
-# Create the client socket
-sock=BluetoothSocket( RFCOMM )
-sock.connect((host, port))
+    addr = None
 
-print("connected.  type stuff")
-while True:
-    data = input()
-    if len(data) == 0: break
-    sock.send(data)
+    if len(sys.argv) < 2:
+        print("No device specified.  Searching all nearby bluetooth devices for the SampleServer service.")
+    else:
+        addr = sys.argv[1]
+        print("Searching for SampleServer on %s" % addr)
 
-sock.close()
+    # search for the SampleServer service
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+    service_matches = find_service( uuid = uuid, address = addr )
+
+    if len(service_matches) == 0:
+        print("Couldn't find the SampleServer service =(")
+        sys.exit(0)
+
+    first_match = service_matches[0]
+    port = first_match["port"]
+    name = first_match["name"]
+    host = first_match["host"]
+
+    print("Connecting to \"%s\" on %s" % (name, host))
+
+    # Create the client socket
+    sock=BluetoothSocket( RFCOMM )
+    sock.connect((host, port))
+
+    text_editor = texteditor.Text_editor_program()
+
+    receiver = Receiver(sock, text_editor)
+    sender = Sender()
+
+    text_editor.mainloop()
+
+    receiver.stop()
+    sender.stop()
+    receiver.join()
+    sender.join()
+
+    sock.close()
+
+'''
+def listen(sock, should_close):
+    try:
+        while True:
+            data = sock.recv(1024)
+            if len(data) == 0:
+                print('listen should close')
+                should_close = True
+                break
+            print('Received {}'.format(data))
+    except IOError:
+        pass
+
+def send(sock, should_close):
+    while True:
+        data = input()
+        if len(data) == 0:
+            print('send should close')
+            should_close = True
+            break
+        sock.send(data)
+
+if __name__ == '__main__':
+    if sys.version < '3':
+        input = raw_input
+
+    addr = None
+
+    if len(sys.argv) < 2:
+        print("No device specified.  Searching all nearby bluetooth devices for the SampleServer service.")
+    else:
+        addr = sys.argv[1]
+        print("Searching for SampleServer on %s" % addr)
+
+    # search for the SampleServer service
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+    service_matches = find_service( uuid = uuid, address = addr )
+
+    if len(service_matches) == 0:
+        print("Couldn't find the SampleServer service =(")
+        sys.exit(0)
+
+    first_match = service_matches[0]
+    port = first_match["port"]
+    name = first_match["name"]
+    host = first_match["host"]
+
+    print("Connecting to \"%s\" on %s" % (name, host))
+
+    # Create the client socket
+    sock=BluetoothSocket( RFCOMM )
+    sock.connect((host, port))
+
+    should_close = False
+    t1 = threading.Thread(target=listen, args=(sock, should_close,))
+    t2 = threading.Thread(target=send, args=(sock, should_close,))
+    t1.start()
+    t2.start()
+
+    print("Connected.  Type stuff:")
+    while True:
+        if should_close == True:
+            t1.join()
+            t2.join()
+            sock.close()
+            break
+'''
