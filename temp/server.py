c04548f7a55f6419a43d5e90a2f9987b34ac7df6
timattil <timattil@student.oulu.fi>
2018-03-06 13:27:38 +0200

    Fixed temp/server code.

--- temp/server.py
+++ temp/server.py
@@ -2,6 +2,7 @@ from bluetooth import *
 import threading as th
 
 def connect(clients):
+    size = 1024
     server_sock=BluetoothSocket( RFCOMM )
     server_sock.bind(("",PORT_ANY))
     server_sock.listen(1)
@@ -19,24 +20,19 @@ def connect(clients):
 
     print("Waiting for connection on RFCOMM channel %d" % port)
 
-    while True:
-        try:
-            client_sock, client_info = server_sock.accept()
-            print("Accepted connection from ", client_info)
-            clients.append(client_sock)
-            while True:
-                data = client_sock.recv(size)
-                if data:
-                    print(data)
-                    client_sock.send(data)
-                except:	
-                    print("Closing socket")	
-                    client_sock.close()
-                    #server_sock.close()
-        except:
-            print("Closing socket")
-            client.close()
-            s.close()
+    try:
+        client_sock, client_info = server_sock.accept()
+        print("Accepted connection from ", client_info)
+        clients.append(client_sock)
+        while True:
+            data = client_sock.recv(size)
+            if data:
+                print(data)
+                client_sock.send(data)
+    except:
+        print("Closing socket")
+        client_sock.close()
+        server_sock.close()
 
 if __name__ == '__main__':
     clients = []
