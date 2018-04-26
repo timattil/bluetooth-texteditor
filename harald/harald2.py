0e5dde9055187c771b23c087e276c76ae98d19fb
nikolehto <niko.lehto@mbnet.fi>
2018-04-26 15:39:03 +0300

    error handling for receive

--- harald/harald2.py
+++ harald/harald2.py
@@ -252,19 +252,21 @@ class Harald():
                             header = string_data[:header_end-1]
                             msg = string_data[header_end:]
                             msg_length = int(header[len("HAR"):-(len("ALD")-1)])
-                            print('GOT', string_data)
-                            print('Message Length', msg_length)
                             remaining = msg_length - 1024 + len(header)
-                            print('remaining', remaining)
                         else:
                             msg += string_data
                             remaining = remaining - 1024
-                            print('add text - remaining', remaining)
                             
                         if remaining <= 0:
-                            print('GOT whole message, remaining', remaining)
-                            formatted_data = format_message(msg)
-                            self.socket_recv_queue.put(formatted_data)
+                            try:
+                                formatted_data = format_message(msg)
+                                self.socket_recv_queue.put(formatted_data)
+                            except ValueError as e:
+                                print(e)
+                                print("Don't worry, all will be ok..")
+                                pass
+                            
+                            
                             whole_msg_recvd = True
 
                         #print("Harald received:", string_data)
@@ -310,7 +312,14 @@ class Harald():
             data = sock.recv(1024)
             if data:
                 string_data = data.decode('utf-8')
-                formatted_data = format_message(string_data)
+                formatted_data = {}
+                try:
+                    formatted_data = format_message(string_data)
+                except ValueError as e:
+                    print(e)
+                    print("Don't worry, all will be ok..")
+                    pass
+                    
                 if formatted_data.get("_type") == "authentication" and formatted_data.get("message") == "granted":
                     return True
                 else:
